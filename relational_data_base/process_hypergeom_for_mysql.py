85713cfacd1e9bac2539900c1a21f9b3aa5b8732
Ubuntu <felixfrancier@gmail.com>
2017-01-11 21:06:07 +0000

    remove genenames from output

--- relational_data_base/process_hypergeom_for_mysql.py
+++ relational_data_base/process_hypergeom_for_mysql.py
@@ -11,23 +11,22 @@ import re
 import numpy as np
 
 
-input_file = "sample_output_web_interface.txt"
-#input_file = "../final_output_10m_011116.txt"
+#input_file = "sample_output_web_interface.txt"
+input_file = "../final_output_10m_011116.txt"
 
-out_put_file1 = "gene_numbering.txt"
-out_put_file2 = "whole_output_with_numbering.txt"
+out_put_file1 = "gene_numbering_011116.txt"
+out_put_file2 = "whole_output_with_numbering_011116.txt"
 df = pd.read_csv(input_file, sep='\t')
 
 
 #print df
 GeneA_col = df['GeneA'].tolist()
 GeneB_col = df['GeneB'].tolist()
-
 combined_list_genes = list(set(GeneA_col + GeneB_col))
-
 n =  int(len(combined_list_genes)+1)
-
 number_list = range(1, n)
+
+
 
 df_gene_names = pd.DataFrame(combined_list_genes, columns=['Gene_name'])
 
@@ -41,16 +40,24 @@ df_gene_names_A = df_gene_names.rename(columns={ 'Gene_name' : 'GeneA', 'Gene_nu
 dfA = pd.merge(df, df_gene_names_A, on='GeneA', how='inner')
 
 df_gene_names_B = df_gene_names.rename(columns={ 'Gene_name' : 'GeneB', 'Gene_number' : 'GeneB_number'})
-#df_gene_names_B = df_gene_names.rename(columns={ 'Gene_name' : 'GeneB', 'Gene_number' : 'GeneB_number'})
 
 #print df_gene_names_B
 #
 dfAB = pd.merge(dfA, df_gene_names_B, on='GeneB', how='inner')
-##
-#print dfAB
+
+
+### select only gene identifier and p_value.
+dfAB_subset = dfAB[['GeneA_number', 'GeneB_number', 'p_value']]
+
+dfAB_subset.index += 1 
+
+
 
 ### print output2 module1 output with gene names converted to numbers
-dfAB.to_csv(out_put_file2, header=None, index=None, sep='\t', mode='a')
+#dfAB.to_csv(out_put_file2, header=None, index=None, sep='\t', mode='a')
+dfAB_subset.to_csv(out_put_file2, header=None, index=True, sep='\t', mode='a')
+
+
 
 
 
